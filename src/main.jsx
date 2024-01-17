3cc677cf2c77375a1804eb525e5b512beb48c8b3
Jair <jairaceves56@gmail.com>
2024-01-17 14:28:29 -0600

    Envolvimos el Provider para manejar el Satate

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { InvoiceApp } from './components/InvoiceApp.jsx'
+import { FacturasProvider } from './context/FacturasProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <InvoiceApp />
+    <FacturasProvider>
+      <InvoiceApp />
+    </FacturasProvider>
   </React.StrictMode>,
 )
