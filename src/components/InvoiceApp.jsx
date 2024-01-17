cb1702642bfba17e30d41fb0ba728069840db62f
Jair <jairaceves56@gmail.com>
2024-01-17 10:26:55 -0600

    Agregamos los datos al Satate

--- src/components/InvoiceApp.jsx
+++ src/components/InvoiceApp.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { invoice } from "../data/invoice"
 import { getInvoice } from "../services/getInvoice"
 import { InvoiceInfo } from "./InvoiceInfo"
@@ -5,9 +6,28 @@ import { InvoiceView } from "./InvoiceView"
 import { TableProducts } from "./TableProducts"
 import { TotalView } from "./TotalView"
 
+
 export const InvoiceApp = () => {
 
-    const { id, name, client, company, items, total} = getInvoice()
+    const { id, name, client, company, items: itemsInitial, total} = getInvoice()
+    const [productValue, setProductValue] = useState('')
+    const [priceValue, setPriceValue] = useState(0)
+    const [quantityValue, setQuantityValue] = useState(0)
+
+    const [items, setItems] = useState(itemsInitial)
+
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        setItems([...items, {
+            key: 4,
+            product: productValue, 
+            price: priceValue, 
+            quantity: quantityValue
+        }])
+
+    }
 
   return (
     <div className="container mx-auto mt-4 rounded-md shadow-xl bg-white">
@@ -43,32 +63,53 @@ export const InvoiceApp = () => {
                         total={total}
                     />
 
-                    {/* Button */}
-                    <div className="p-2">
-                        <button className="bg-gray-500 uppercase p-1 shadow-lg text-white w-24 rounded-md duration-300 hover:bg-gray-600">Add Item</button>
-                    </div>
-
-                    <form className="flex flex-col space-y-3 p-3 border rounded-md">
+                    <form 
+                         onSubmit={handleSubmit}
+                        className="flex flex-col space-y-3 p-3 border rounded-md mt-10">
                         <h2 className="text-2xl text-center font-extrabold text-gray-600">Agregar Factura</h2>
                         <input 
                             type="text" 
                             name="product" 
                             placeholder="Producto"
                             className="p-2 border-2 rounded-md" 
+                            onChange={ e => {
+                                e.target.value 
+                                setProductValue(e.target.value)
+                            }}
+                           
                         />
                         <input 
                             type="text" 
                             name="price" 
                             placeholder="Precio" 
-                            className="p-2 border-2 rounded-md"  
+                            className="p-2 border-2 rounded-md"
+                            onChange={ e => {
+                                e.target.value 
+                                setPriceValue(e.target.value)
+                            }}  
                         />
                         <input 
                             type="text" 
                             name="quantity" 
                             placeholder="Cantidad" 
-                            className="p-2 border-2 rounded-md" 
+                            className="p-2 border-2 rounded-md"
+                            onChange={ e => {
+                                e.target.value 
+                                setQuantityValue(e.target.value)
+                            }} 
                         />
+                   
+                        {/* Button */}
+                        <div className="p-2">
+                            <button
+                                type="submit"
+                                className="bg-gray-500 uppercase p-2 shadow-lg text-white w-full rounded-md duration-300 hover:bg-gray-600"
+                            >
+                                Add Item
+                            </button>
+                        </div>
                     </form>
+
                 </div>
             </div>
         </div>
