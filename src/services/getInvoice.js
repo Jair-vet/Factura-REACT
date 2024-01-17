a0a5dad415da4659112f87fedd91aeba1c70af9a
Jair <jairaceves56@gmail.com>
2024-01-17 09:25:43 -0600

    Creamos la funcion reducer

--- src/services/getInvoice.js
+++ src/services/getInvoice.js
@@ -2,6 +2,15 @@ import { invoice } from "../data/invoice"
 
 export const getInvoice = () => {
 
+    // let total = 0;
+    // invoice.items.forEach( item => {
+    //     total = total + item.price * item.quantity
+    // })
 
-    return invoice
+    const total = invoice.items
+            .map( item => item.price * item.quantity )  // retorna un arreglo tipo number
+            .reduce( (accumulator, currentValue) => accumulator + currentValue, 0 )
+
+
+    return {...invoice, total}
 }
